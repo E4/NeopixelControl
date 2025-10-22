3175a098629ac2c75bdb8e301f1dcace5bd8caa5
Emre Yucel <e4emre@gmail.com>
2025-10-22 08:02:59 -0400

    change to http server model

--- main/main.c
+++ main/main.c
@@ -8,20 +8,18 @@
 #include "nvs_flash.h"
 #include "lwip/err.h"
 #include "lwip/sys.h"
-#include "esp_http_client.h"
+#include "esp_netif.h"
+#include "esp_http_server.h"
 #include "esp_tls.h"
 
 #include "driver/gpio.h"
 #include "neopixel.h"
 
-//#include "led_strip.h"
+#include "chaser_data.h"
 
 // define the following:
 // CONFIG_WIFI_SSID
 // CONFIG_WIFI_PASSWORD
-// CONFIG_HTTP_HOST
-// CONFIG_HTTP_PORT
-// CONFIG_HTTP_PATH
 
 // CONFIG_LED_GPIO
 // CONFIG_LED_COUNT
@@ -34,294 +32,213 @@
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 #define MAX_HTTP_OUTPUT_BUFFER 5000
 
-typedef struct  {
-    uint8_t color[18];        //  0-17: Six 24-bit colors in RGB triplets
-    uint16_t position_offset; // 18-19: Starting position of this chaser
-    int8_t position_speed;    //    20: The direction and speed of motion
-    uint8_t color_offset;     //    21: Starting color of this chaser (0 to 239)
-    uint8_t color_speed;      //    22: The speed of color shift
-    uint8_t position_delay;   //    23: Move position every n'th frame (minimum 1)
-    uint8_t color_delay;      //    24: Move color every n'th frame (minimum 1)
-    uint8_t repeat;           //    25: Apply this to every n'th pixel (0 = no repeat)
-    uint16_t limit_low;       // 26-27: Not implemented
-    uint16_t limit_high;      // 28-29: Not implemented
-    uint8_t reserved[2];      // 30-31: Not implemented
-} chaser_data_t;
+
+static httpd_handle_t start_webserver(void);
+static void stop_webserver(void);
+static esp_err_t server_request_handler(httpd_req_t *req);
+static void move_chasers();
+static uint32_t get_interpolated_rgb_for_chaser(chaser_data_t *data);
+static void flash_leds(int8_t r, int8_t g, int8_t b);
+static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+static void init_wifi();
 
 
-static int retry_num=0;
 static int wifi_connected = 0;
 static tNeopixelContext neopixel;
 chaser_data_t *chaser_data = NULL;
 tNeopixel chaser_pixel[CONFIG_LED_COUNT];
 int8_t chaser_count = 0;
 
+static httpd_handle_t server = NULL;
+static char html[2048];
+static const char index_html_template[] = "<!DOCTYPE html><html><head><meta charset=\"utf-8\"/><title>Lightie</title><style> html, body { margin: 0; background-color: black; } body { display: flex; flex-direction: column; align-items: center; min-height: 100svmin; } .top { width: 100svmin; text-align: center; padding: 10svmin 0; height: 20svmin; } .sliders { display: flex; align-items: center; justify-content: center; width: 100svmin; height: 60svmin; padding: 5svmin; box-sizing: border-box; } input[type=\"range\"] { writing-mode: bt-lr; -webkit-appearance: slider-vertical; height: 100%%; flex: 1 1 0; min-width: 0; } #c { width: 50svmin; height: 20svmin; border: none; outline: none; cursor: pointer; } </style></head><body><div class=\"top\"><input id=\"c\" type=\"color\"></div><div class=\"sliders\"><input id=\"r\" type=\"range\" min=\"0\" max=\"255\" value=\"0\"/><input id=\"g\" type=\"range\" min=\"0\" max=\"255\" value=\"0\"/><input id=\"b\" type=\"range\" min=\"0\" max=\"255\" value=\"0\"/></div><script>const c=()=>\"#\"+(16777216|a.r<<16|a.g<<8|a.b).toString(16).slice(1),d=document.getElementById(\"r\"),e=document.getElementById(\"g\"),f=document.getElementById(\"b\"),g=document.getElementById(\"c\");let h,a={r:%d,g:%d,b:%d},k=0;function l(b){b.target==g?(b=g.value,b=b.replace(/^#/,\"\"),b=parseInt(b,16),a={r:b>>16&255,g:b>>8&255,b:b&255},d.value=a.r,e.value=a.g,f.value=a.b):(a.r=d.value,a.g=e.value,a.b=f.value,g.value=c());performance.now()>k+100?m():(clearTimeout(h),h=setTimeout(m,k+100-performance.now()))}function m(){k=performance.now();fetch(`/?r=${a.r}&g=${a.g}&b=${a.b}`,{method:\"GET\",cache:\"no-store\",keepalive:!0}).catch(()=>{})}[d,e,f,g].forEach(b=>b.addEventListener(\"input\",l));g.value=c();d.value=a.r;e.value=a.g;f.value=a.b;</script></body></html>";
 
-static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id,void *event_data) {
-    if(event_id == WIFI_EVENT_STA_START) {
-        ESP_LOGI(TAG, "WiFi starting");
-    } else if (event_id == WIFI_EVENT_STA_CONNECTED) {
-        ESP_LOGI(TAG, "WiFi connected");
-    } else if (event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        ESP_LOGI(TAG, "WiFi disconnected");
-        wifi_connected = 0;
-        if(retry_num<5) {
-            esp_wifi_connect();
-            retry_num++;
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            ESP_LOGI(TAG, "WiFi request connection");
-        }
-    } else if (event_id == IP_EVENT_STA_GOT_IP) {
-        ESP_LOGI(TAG, "WiFi got IP address");
-        wifi_connected = 1;
-    }
-}
-
-
-void init_wifi()
-{
-    esp_netif_init();
-    esp_event_loop_create_default();
-    esp_netif_create_default_wifi_sta();
-    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&wifi_initiation);
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
-    wifi_config_t wifi_configuration = {
-        .sta = {
-            .ssid = CONFIG_WIFI_SSID,
-            .password = CONFIG_WIFI_PASSWORD,
-           }
-       };
-    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
-
-    esp_wifi_start();
-    esp_wifi_set_mode(WIFI_MODE_STA);
+static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    ESP_LOGI(TAG, "WiFi starting");
     esp_wifi_connect();
-}
-
-
-esp_err_t _http_event_handler(esp_http_client_event_t *evt)
-{
-    static char *output_buffer;  // Buffer to store response of http request from event handler
-    static int output_len;       // Stores number of bytes read
-    switch(evt->event_id) {
-        case HTTP_EVENT_ERROR:
-            ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
-            break;
-        case HTTP_EVENT_ON_CONNECTED:
-            ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
-            break;
-        case HTTP_EVENT_HEADER_SENT:
-            ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
-            break;
-        case HTTP_EVENT_ON_HEADER:
-            ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
-            break;
-        case HTTP_EVENT_ON_DATA:
-            ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-            // Clean the buffer in case of a new request
-            if (output_len == 0 && evt->user_data) {
-                // we are just starting to copy the output data into the use
-                memset(evt->user_data, 0, MAX_HTTP_OUTPUT_BUFFER);
-            }
-            /*
-             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
-             *  However, event handler can also be used in case chunked encoding is used.
-             */
-            if (!esp_http_client_is_chunked_response(evt->client)) {
-                // If user_data buffer is configured, copy the response into the buffer
-                int copy_len = 0;
-                if (evt->user_data) {
-                    // The last byte in evt->user_data is kept for the NULL character in case of out-of-bound access.
-                    copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
-                    if (copy_len) {
-                        memcpy(evt->user_data + output_len, evt->data, copy_len);
-                    }
-                } else {
-                    int content_len = esp_http_client_get_content_length(evt->client);
-                    if (output_buffer == NULL) {
-                        // We initialize output_buffer with 0 because it is used by strlen() and similar functions therefore should be null terminated.
-                        output_buffer = (char *) calloc(content_len + 1, sizeof(char));
-                        output_len = 0;
-                        if (output_buffer == NULL) {
-                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
-                            return ESP_FAIL;
-                        }
-                    }
-                    copy_len = MIN(evt->data_len, (content_len - output_len));
-                    if (copy_len) {
-                        memcpy(output_buffer + output_len, evt->data, copy_len);
-                    }
-                }
-                output_len += copy_len;
-            }
-            break;
-        case HTTP_EVENT_ON_FINISH:
-            ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
-            if (output_buffer != NULL) {
-                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
-                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
-                free(output_buffer);
-                output_buffer = NULL;
-            }
-            output_len = 0;
-            break;
-        case HTTP_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
-            int mbedtls_err = 0;
-            esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)evt->data, &mbedtls_err, NULL);
-            if (err != 0) {
-                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
-                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
-            }
-            if (output_buffer != NULL) {
-                free(output_buffer);
-                output_buffer = NULL;
-            }
-            output_len = 0;
-            break;
-        case HTTP_EVENT_REDIRECT:
-            ESP_LOGI(TAG, "HTTP_EVENT_REDIRECT");
-            esp_http_client_set_header(evt->client, "From", "user@example.com");
-            esp_http_client_set_header(evt->client, "Accept", "text/html");
-            esp_http_client_set_redirection(evt->client);
-            break;
+  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    ESP_LOGI(TAG, "WiFi disconnected");
+    esp_wifi_connect();
+    if (server) {
+      stop_webserver();
     }
-    return ESP_OK;
-}
-
-
-static void http_rest_with_url(void)
-{
-    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER + 1] = {0};
-    int content_len;
-    uint8_t i;
-    esp_http_client_config_t config = {
-        .host = CONFIG_HTTP_HOST,
-        .port = CONFIG_HTTP_PORT,
-        .path = CONFIG_HTTP_PATH,
-        .query = "",
-        .event_handler = _http_event_handler,
-        .user_data = local_response_buffer,        // Pass address of local buffer to get response
-        .disable_auto_redirect = true,
-    };
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-
-    // GET
-    esp_err_t err = esp_http_client_perform(client);
-    if (err == ESP_OK) {
-        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %"PRId64,
-                esp_http_client_get_status_code(client),
-                esp_http_client_get_content_length(client));
-    } else {
-        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ESP_LOGI(TAG, "Got IP address");
+    if (server == NULL) {
+      server = start_webserver();
     }
-
-    content_len = esp_http_client_get_content_length(client);
-
-    ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, content_len); // this is the data
-
-    chaser_data = (chaser_data_t*) local_response_buffer;
-    chaser_count = content_len / 32;
-    for(i=0;i<chaser_count;i++) {
-        if(chaser_data[i].position_delay==0) chaser_data[i].position_delay++;
-        if(chaser_data[i].color_delay==0) chaser_data[i].color_delay++;
-    }
-    esp_http_client_cleanup(client);
+  }
 }
 
 
-
-static void http_test_task(void *pvParameters)
+static void init_wifi()
 {
-    http_rest_with_url();
-    ESP_LOGI(TAG, "HTTP requests completed");
-    vTaskDelete(NULL);
-}
+  esp_netif_init();
+  esp_event_loop_create_default();
+  esp_netif_create_default_wifi_sta();
+  wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
+  esp_wifi_init(&wifi_initiation);
+  esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
+  esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
+  wifi_config_t wifi_configuration = {
+    .sta = {
+      .ssid = CONFIG_WIFI_SSID,
+      .password = CONFIG_WIFI_PASSWORD,
+       }
+     };
+  esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
 
+  esp_wifi_start();
+  esp_wifi_set_mode(WIFI_MODE_STA);
+  esp_wifi_connect();
+}
 
 
 static void flash_leds(int8_t r, int8_t g, int8_t b) {
-    for(int i=0;i<CONFIG_LED_COUNT;i++) {
-        // led_strip_set_pixel(led_strip, i, r, g, b);
-    }
-    // led_strip_refresh(led_strip);
-    vTaskDelay(5 / portTICK_PERIOD_MS);
-    // led_strip_clear(led_strip);
+  for(int i=0;i<CONFIG_LED_COUNT;i++) {
+    // led_strip_set_pixel(led_strip, i, r, g, b);
+  }
+  // led_strip_refresh(led_strip);
+  vTaskDelay(5 / portTICK_PERIOD_MS);
+  // led_strip_clear(led_strip);
 }
 
 
 
-static uint32_t get_interpolated_rgb_for_chaser(chaser_data_t *data) {
-    static int8_t i0;
-    static int8_t i1;
-    static int8_t interpolator;
-    i0 = data->color_offset / 40;
-    i1 = ((i0 + 1) % 6)*3;
-    i0 *= 3;
-    interpolator = data->color_offset % 40;
-    return NP_RGB(
-        ((data->color[i1+0]-data->color[i0+0])*interpolator/40)+data->color[i0+0],
-        ((data->color[i1+1]-data->color[i0+1])*interpolator/40)+data->color[i0+1],
-        ((data->color[i1+2]-data->color[i0+2])*interpolator/40)+data->color[i0+2]
-    );
+
+
+
+static uint32_t get_interpolated_rgb_for_chaser(chaser_data_t *data)
+{
+  static int8_t i0;
+  static int8_t i1;
+  static int8_t interpolator;
+  i0 = data->color_offset / 40;
+  i1 = ((i0 + 1) % 6)*3;
+  i0 *= 3;
+  interpolator = data->color_offset % 40;
+  return NP_RGB(
+    ((data->color[i1+0]-data->color[i0+0])*interpolator/40)+data->color[i0+0],
+    ((data->color[i1+1]-data->color[i0+1])*interpolator/40)+data->color[i0+1],
+    ((data->color[i1+2]-data->color[i0+2])*interpolator/40)+data->color[i0+2]
+  );
 }
 
 
-static void move_chasers() {
-    static uint32_t frame;
-    static int16_t i;
-    static int16_t repeats;
-    static int16_t r;
-    static uint32_t chaser_color;
-    if(++frame==0) frame=1;
-    for(i=0;i<chaser_count;i++) {
-        if(frame%chaser_data[i].position_delay==0) chaser_data[i].position_offset = (chaser_data[i].position_offset + chaser_data[i].position_speed) % CONFIG_LED_COUNT;
-        if(frame%chaser_data[i].color_delay==0) chaser_data[i].color_offset = (chaser_data[i].color_offset + chaser_data[i].color_speed) % 240;
-        chaser_color = get_interpolated_rgb_for_chaser(&chaser_data[i]);
-        if(chaser_data[i].repeat) {
-            repeats = CONFIG_LED_COUNT/chaser_data[i].repeat;
-            for(r=0;r<repeats;r++) {
-                chaser_pixel[r].index = (chaser_data[i].position_offset + chaser_data[i].repeat * r) % CONFIG_LED_COUNT;
-                chaser_pixel[r].rgb = chaser_color;
-            }
-            neopixel_SetPixel(neopixel, chaser_pixel, r);
-        } else {
-            chaser_pixel[0].index = chaser_data[i].position_offset;
-            chaser_pixel[0].rgb = chaser_color;
-            neopixel_SetPixel(neopixel, chaser_pixel, 1);
-        }
+static void move_chasers()
+{
+  static uint32_t frame;
+  static int16_t i;
+  static int16_t repeats;
+  static int16_t r;
+  static uint32_t chaser_color;
+  if(++frame==0) frame=1;
+  for(i=0;i<chaser_count;i++) {
+    if(frame%chaser_data[i].position_delay==0) chaser_data[i].position_offset = (chaser_data[i].position_offset + chaser_data[i].position_speed) % CONFIG_LED_COUNT;
+    if(frame%chaser_data[i].color_delay==0) chaser_data[i].color_offset = (chaser_data[i].color_offset + chaser_data[i].color_speed) % 240;
+    chaser_color = get_interpolated_rgb_for_chaser(&chaser_data[i]);
+    if(chaser_data[i].repeat) {
+      repeats = CONFIG_LED_COUNT/chaser_data[i].repeat;
+      for(r=0;r<repeats;r++) {
+        chaser_pixel[r].index = (chaser_data[i].position_offset + chaser_data[i].repeat * r) % CONFIG_LED_COUNT;
+        chaser_pixel[r].rgb = chaser_color;
+      }
+      neopixel_SetPixel(neopixel, chaser_pixel, r);
+    } else {
+      chaser_pixel[0].index = chaser_data[i].position_offset;
+      chaser_pixel[0].rgb = chaser_color;
+      neopixel_SetPixel(neopixel, chaser_pixel, 1);
     }
+  }
+}
+
+
+
+static esp_err_t server_request_handler(httpd_req_t *req)
+{
+  char query[256];
+  bool has_rgb = false;
+  if (httpd_req_get_url_query_str(req, query, sizeof(query)) == ESP_OK) {
+    ESP_LOGI(TAG, "Request query: %s", query);
+    char value[8];
+    if (httpd_query_key_value(query, "r", value, sizeof(value)) == ESP_OK) {
+      atoi(value);
+      has_rgb = true;
+    }
+    if (httpd_query_key_value(query, "g", value, sizeof(value)) == ESP_OK) {
+      atoi(value);
+      has_rgb = true;
+    }
+    if (httpd_query_key_value(query, "b", value, sizeof(value)) == ESP_OK) {
+      atoi(value);
+      has_rgb = true;
+    }
+  }
+  if (has_rgb) {
+    const char resp[] = "OK\n";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+  } else {
+    httpd_resp_set_type(req, "text/html");
+    int len = snprintf(html, sizeof(html), index_html_template, 0, 0, 0);
+    httpd_resp_send(req, html, len);
+  }
+  return ESP_OK;
+}
+
+static httpd_handle_t start_webserver(void)
+{
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  // Increase stack size to handle HTML generation and logging
+  config.stack_size = 8192;
+  httpd_handle_t server = NULL;
+  if (httpd_start(&server, &config) == ESP_OK) {
+    httpd_uri_t uri = {
+      .uri = "/",
+      .method = HTTP_GET,
+      .handler = server_request_handler,
+      .user_ctx = NULL
+    };
+    httpd_register_uri_handler(server, &uri);
+  }
+  return server;
+}
+
+
+static void stop_webserver(void)
+{
+  if (server) {
+    httpd_stop(server);
+    server = NULL;
+  }
 }
 
 
 
 
 void app_main(void) {
-    nvs_flash_init();
-    init_wifi();
-    neopixel = neopixel_Init(CONFIG_LED_COUNT, CONFIG_LED_GPIO);
-    while(wifi_connected==0) {
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        flash_leds(32,0,0);
-    }
+  nvs_flash_init();
+  init_wifi();
+  neopixel = neopixel_Init(CONFIG_LED_COUNT, CONFIG_LED_GPIO);
+  while(wifi_connected==0) {
     vTaskDelay(500 / portTICK_PERIOD_MS);
-    xTaskCreate(&http_test_task, "http_request", 8192, NULL, 5, NULL);
-    ESP_LOGI(TAG, "HTTP request complete!");
+    flash_leds(32,0,0);
+  }
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  ESP_LOGI(TAG, "HTTP request complete!");
 
-    while(chaser_count == 0) {
-        ESP_LOGI(TAG, "chaser count: %d", chaser_count);
-        flash_leds(0,0,32);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
-    flash_leds(0,32,0);
+  while(chaser_count == 0) {
+    ESP_LOGI(TAG, "chaser count: %d", chaser_count);
+    flash_leds(0,0,32);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+  flash_leds(0,32,0);
 
-    while(1) {
-        move_chasers();
-        vTaskDelay(14 / portTICK_PERIOD_MS);
-    }
-    neopixel_Deinit(neopixel);
+  while(1) {
+    move_chasers();
+    vTaskDelay(14 / portTICK_PERIOD_MS);
+  }
+  neopixel_Deinit(neopixel);
 }
 
 
